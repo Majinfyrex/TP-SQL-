8c9a7ee6d54655217e7b9707ef48e51eed776a2c
Zakary Ben El Gharib <zakary34750@gmail.com>
2024-10-21 15:20:41 +0200

    creation des tables pour le tp02 SQL

---
+++ tp02.sql
@@ -0,0 +1,14 @@
+INSERT INTO FOURNISSEUR (ID, NOM) values (1, "Française d'Imports");
+INSERT INTO FOURNISSEUR (ID, NOM) values (2, "FDM SA");
+INSERT INTO FOURNISSEUR (ID, NOM) values (3, "Dubois & Fils");
+
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (1, "A01", 'Perceuse P1', 74.99, "Française d'Imports");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (2, "F01", 'Boulon laiton 4 x 40 mm (sachet de 10)', 2.25, "FDM SA");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (3, "F02", 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, "FDM SA");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (4, "D01", 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.40, "DUBOIS & FILS");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (5, "A02", 'Meuleuse 125mm', 37.85, "Française d'Imports");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (6, "D03", 'Boulon acier zingué 4 x 40 mm (sachet de 10)', 1.8, "DUBOIS & FILS");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (7, "A03", 'Perceuse à colonne', 4.45, "FDM SA");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (8, "D04", 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, "FDM SA");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (9, "F03", 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, "FDM SA");
+INSERT INTO ARTCILES (ID, REF, DESIGNATION, PRIX, ID_FOU) values (10, "F04", 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, "FDM SA");
